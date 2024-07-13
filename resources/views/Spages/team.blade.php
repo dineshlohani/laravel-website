78d2932d5b0a7240439b77e0eaf6bcd09c801851
DineshLohani <dineshlohani333@gmail.com>
2024-07-13 11:00:09 +0545

    update

--- resources/views/Spages/team.blade.php
+++ resources/views/Spages/team.blade.php
@@ -1,10 +1,251 @@
 @extends('layouts.app')
 
-@section('title', 'Shinjiru | Counter Up')
+@section('title', 'Shinjiru Educational | Team')
 
 @section('content')
 
+<div class="rts-breadcrumb-area breadcrumb-bg bg_image">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 breadcrumb-1">
+                    <h1 class="title">Our Team</h1>
+                </div>
+                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                    <div class="bread-tag">
+                        <a href="index-2.html">Home</a>
+                        <span> / </span>
+                        <a href="#" class="active">Our Team</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end breadcrumb area -->
 
+
+    <!-- team area start-->
+    <div class="rts-team-area rts-section-gap bg-team-color">
+        <div class="container">
+            <div class="row g-5">
+                <!-- team single start -->
+                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
+                    <div class="team-single-one-start">
+                        <div class="team-image-area">
+                            <a href="team-details.html">
+                                <img src="assets/images/team/tm/06.jpg" alt="Business_Team_single">
+                                <div class="team-social">
+                                    <div class="main">
+                                        <i class="fal fa-plus"></i>
+                                    </div>
+                                    <div class="team-social-one">
+                                        <i class="fab fa-youtube"></i>
+                                        <i class="fab fa-twitter"></i>
+                                        <i class="fab fa-instagram"></i>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="single-details">
+                            <a href="team-details.html">
+                                <h5 class="title">Bishal Dhamala</h5>
+                            </a>
+                            <p>CEO</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- team single end -->
+                <!-- team single start -->
+                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
+                    <div class="team-single-one-start">
+                        <div class="team-image-area">
+                            <a href="team-details.html">
+                                <img src="assets/images/team/tm/07.jpg" alt="Business_Team_single">
+                                <div class="team-social">
+                                    <div class="main">
+                                        <i class="fal fa-plus"></i>
+                                    </div>
+                                    <div class="team-social-one">
+                                        <i class="fab fa-youtube"></i>
+                                        <i class="fab fa-twitter"></i>
+                                        <i class="fab fa-instagram"></i>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="single-details">
+                            <a href="team-details.html">
+                                <h5 class="title">Dinesh Lohani</h5>
+                            </a>
+                            <p>Co-Founder</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- team single end -->
+                <!-- team single start -->
+                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
+                    <div class="team-single-one-start">
+                        <div class="team-image-area">
+                            <a href="team-details.html">
+                                <img src="assets/images/team/tm/08.jpg" alt="Business_Team_single">
+                                <div class="team-social">
+                                    <div class="main">
+                                        <i class="fal fa-plus"></i>
+                                    </div>
+                                    <div class="team-social-one">
+                                        <i class="fab fa-youtube"></i>
+                                        <i class="fab fa-twitter"></i>
+                                        <i class="fab fa-instagram"></i>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="single-details">
+                            <a href="team-details.html">
+                                <h5 class="title">Indira Lohani</h5>
+                            </a>
+                            <p>Consultant</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- team single end -->
+                <!-- team single start -->
+                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
+                    <div class="team-single-one-start">
+                        <div class="team-image-area">
+                            <a href="team-details.html">
+                                <img src="assets/images/team/tm/09.jpg" alt="Business_Team_single">
+                                <div class="team-social">
+                                    <div class="main">
+                                        <i class="fal fa-plus"></i>
+                                    </div>
+                                    <div class="team-social-one">
+                                        <i class="fab fa-youtube"></i>
+                                        <i class="fab fa-twitter"></i>
+                                        <i class="fab fa-instagram"></i>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="single-details">
+                            <a href="team-details.html">
+                                <h5 class="title">Prestige Burlakoti </h5>
+                            </a>
+                            <p> Manager</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- team single end -->
+                <!-- team single start -->
+                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
+                    <div class="team-single-one-start">
+                        <div class="team-image-area">
+                            <a href="team-details.html">
+                                <img src="assets/images/team/tm/10.jpg" alt="Business_Team_single">
+                                <div class="team-social">
+                                    <div class="main">
+                                        <i class="fal fa-plus"></i>
+                                    </div>
+                                    <div class="team-social-one">
+                                        <i class="fab fa-youtube"></i>
+                                        <i class="fab fa-twitter"></i>
+                                        <i class="fab fa-instagram"></i>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="single-details">
+                            <a href="team-details.html">
+                                <h5 class="title">Bikash Dhamala</h5>
+                            </a>
+                            <p>Finance Manager</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- team single end -->
+                <!-- team single start -->
+                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
+                    <div class="team-single-one-start">
+                        <div class="team-image-area">
+                            <a href="team-details.html">
+                                <img src="assets/images/team/tm/11.jpg" alt="Business_Team_single">
+                                <div class="team-social">
+                                    <div class="main">
+                                        <i class="fal fa-plus"></i>
+                                    </div>
+                                    <div class="team-social-one">
+                                        <i class="fab fa-youtube"></i>
+                                        <i class="fab fa-twitter"></i>
+                                        <i class="fab fa-instagram"></i>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="single-details">
+                            <a href="team-details.html">
+                                <h5 class="title">Amelia Clover</h5>
+                            </a>
+                            <p>Language Teacher</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- team single end -->
+                <!-- team single start -->
+                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
+                    <div class="team-single-one-start">
+                        <div class="team-image-area">
+                            <a href="team-details.html">
+                                <img src="assets/images/team/tm/12.jpg" alt="Business_Team_single">
+                                <div class="team-social">
+                                    <div class="main">
+                                        <i class="fal fa-plus"></i>
+                                    </div>
+                                    <div class="team-social-one">
+                                        <i class="fab fa-youtube"></i>
+                                        <i class="fab fa-twitter"></i>
+                                        <i class="fab fa-instagram"></i>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="single-details">
+                            <a href="team-details.html">
+                                <h5 class="title">Beckett Hayden</h5>
+                            </a>
+                            <p>Language Teacher</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- team single end -->
+                <!-- team single start -->
+                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
+                    <div class="team-single-one-start">
+                        <div class="team-image-area">
+                            <a href="team-details.html">
+                                <img src="assets/images/team/tm/13.jpg" alt="Business_Team_single">
+                                <div class="team-social">
+                                    <div class="main">
+                                        <i class="fal fa-plus"></i>
+                                    </div>
+                                    <div class="team-social-one">
+                                        <i class="fab fa-youtube"></i>
+                                        <i class="fab fa-twitter"></i>
+                                        <i class="fab fa-instagram"></i>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="single-details">
+                            <a href="team-details.html">
+                                <h5 class="title">Julian Wyat</h5>
+                            </a>
+                            <p>Finance Manager</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- team single end -->
+            </div>
+        </div>
+    </div>
 
 
 @endsection
