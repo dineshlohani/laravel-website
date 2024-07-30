2fb3dfc74ca90d7d0b0eb7b14a60f92230132cd8
DineshLohani <dineshlohani333@gmail.com>
2024-07-30 11:43:21 +0545

    update

--- api/index.php
+++ api/index.php
@@ -1,23 +1,4 @@
 <?php
 
-// Set the path to your Laravel project directory
-$laravel_project_path = dirname(__DIR__);
-
-// Change the working directory to the Laravel project path
-chdir($laravel_project_path);
-
-// Include the Laravel autoload file
-require $laravel_project_path . '/vendor/autoload.php';
-
-// Run the Laravel application
-$app = require_once $laravel_project_path . '/bootstrap/app.php';
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+// Forward Vercel requests to normal index.php
+require __DIR__ . '/../public/index.php';
